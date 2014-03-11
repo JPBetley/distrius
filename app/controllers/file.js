d269cdaddc0afb2a08377b8e97fc74e36aba2ad8
John Phillip Betley <jpbetley@gmail.com>
2014-03-11 12:00:45 -0400

    file model and controller. still need to finish download generation

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -1,12 +1,31 @@
 var mongoose = require('mongoose'),
+    path = require('path'),
     File = mongoose.model('File');
 
 exports.index = function(req, res) {
     File.find(function(err, files) {
         if(err) throw new Error(err);
-        res.render('files/index', {
-            title: 'Shippit',
-            files: files
-        });
+        res.render('files/index', {});
+    });
+};
+
+exports.create = function(req, res) {
+    var url = req.body.url,
+        name = path.basename(url);
+    File.create({
+        url: url,
+        name: name,
+        ipaddress: req.ip,
+        hostname: req.hostname
+    }, function(createErr, file) {
+        res.redirect('/file/' + file.slug);
+    });
+};
+
+exports.show = function(req, res) {
+    var slug = req.params.slug;
+    File.findOne({ slug: slug}, function(err, file) {
+        if(err) throw new Error(err);
+        res.render('files/show', { file: file });
     });
 };
--- app/models/file.js
+++ app/models/file.js
@@ -1,11 +1,15 @@
 // Example model
 
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+  Schema = mongoose.Schema,
+  slug_service = require('../services/slug_service');
 
 var FileSchema = new Schema({
+    slug: { type: String, default: slug_service.generate_slug() },
     name: String,
-    url: String
+    url: String,
+    ipaddress: String,
+    hostname: String
 });
 
 FileSchema.virtual('date')
