bf8ca5cda5d7085cd3e1666d87b3d3bb0461b5b5
John Phillip Betley <jpbetley@gmail.com>
2014-03-11 19:02:52 -0400

    fix for generate js

--- public/js/generate.js
+++ public/js/generate.js
@@ -1,12 +1,43 @@
-(function() {
+(function($) {
     var Generate = {
         init: function() {
-            cacheElements();
+            this.cacheElements();
+            this.bindEvents();
         },
 
         cacheElements: function() {
-            this.form = document.getElementById('generate-form');
-            this.url_output = document.getElementById('url-output');
+            this.form = $('#generate-form');
+            this.url_output = $('#url-output');
+        },
+
+        bindEvents: function() {
+            this.form.bind('submit', this.formSubmit);
+        },
+
+        formSubmit: function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            Generate.generateLink();
+        },
+
+        generateLink: function() {
+            var data = Generate.form.serialize();
+            $.ajax({
+                type: 'POST',
+                url: '/download',
+                data: data
+            }).done(function(data) {
+                Generate.displayLink(data.slug);
+            });
+        },
+
+        displayLink: function(link) {
+            Generate.url_output.val(window.location.origin + "/download/" + link);
+            Generate.url_output.focus();
+            Generate.url_output.select();
         }
     };
-})();
+
+    Generate.init();
+
+})(jQuery);
