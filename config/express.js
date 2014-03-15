3953452d56f23b61560f936cf8fdbeb0d37a868e
John Phillip Betley <jpbetley@gmail.com>
2014-03-15 23:39:03 +0000

    added csrf protection

--- config/express.js
+++ config/express.js
@@ -11,7 +11,16 @@ module.exports = function(app, config) {
     app.use(express.favicon(config.root + '/public/img/favicon.ico'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: "changeme" }));
+    app.use(express.csrf());
     app.use(express.methodOverride());
+    app.use(function(req, res, next) {
+        res.locals.token = req.session._csrf;
+        next();
+    });
+
+
     app.use(app.router);
     app.use(function(req, res) {
       res.status(404).render('404', { title: '404' });
