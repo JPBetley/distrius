7331649704aa32b172c19b4a895ad632a0e8901f
John Betley <jpbetley@gmail.com>
2014-04-17 11:53:33 -0400

    split out ajax and normal download. added index

--- app/controllers/download.js
+++ app/controllers/download.js
@@ -8,26 +8,68 @@ var mongoose = require('mongoose'),
 exports.download = function(req, res) {
     var slug = req.params.slug;
     Download.findOne({ 'slug': slug }, function (err, download) {
-        console.log(download.download_date);
-        if (download.uses_remaining !== 0) {
-            download.ipaddress = req.ip;
-            download.hostname = req.host;
-            download.uses_remaining = download.uses_remaining - 1;
-            download.download_date = Date.now();
-            download.save(function (saveErr) {
-                if (saveErr) {
-                    console.log(saveErr);
-                    return res.render('404');
-                }
-            });
-
-            res.attachment(download.file_name);
-            request.get(download.file_url).pipe(res);
-
-        } else { 
-            res.render('download/used', {});
+        if (req.xhr) {
+            _ajaxDownload(req, res, download, err);
+        } else {
+            _download(req, res, download, err);
         }
+        
     });
+};
+
+var _streamDownload = function(req, res, download, callback) {
+    download.ipaddress = req.ip;
+    download.hostname = req.host;
+    download.uses_remaining = download.uses_remaining - 1;
+    download.download_date = Date.now();
+    download.save(callback);
+
+    res.attachment(download.file_name);
+    request.get(download.file_url).pipe(res);
+};
+
+var _download = function(req, res, download, err) {
+    if (err) {
+        console.log(err);
+        return res.render('404');
+    }
+
+    if (download.uses_remaining !== 0) {
+        _streamDownload(req, res, download, function (saveErr) {
+            if (saveErr) {
+                console.log(saveErr);
+                return res.render('500');
+            }
+        }); 
+    } else { 
+        res.render('download/used', {});
+    }
+    
+};
+
+var _ajaxDownload = function(req, res, download, err) {
+    if (err) {
+        console.log(err);
+        res.status('404')
+        return res.send({ error: "Download not found" });
+    }
+
+    if (download.uses_remaining !== 0) {
+        _streamDownload(req, res, download, function (saveErr) {
+            if (saveErr) {
+                console.log(saveErr);
+                res.status(500);
+                return res.send({ error: "Download failed." });
+            }
+        }); 
+    } else { 
+        res.status(500);
+        return res.send({ error: "No downloads remaining." });
+    }
+};
+
+exports.index = function(req, res) {
+    res.render('download/index');
 };
 
 exports.create = function(req, res) {
