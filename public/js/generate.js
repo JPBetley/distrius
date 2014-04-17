917b5ea313181bf10554e00c3470bea6a3cf11fd
John Betley <jpbetley@gmail.com>
2014-04-17 13:06:05 -0400

    better module pattern for generate

--- public/js/generate.js
+++ public/js/generate.js
@@ -1,5 +1,5 @@
 var Generate = (function($) {
-    var Generate = {
+    var generate = {
         init: function() {
             this.cacheElements();
             this.bindEvents();
@@ -14,85 +14,91 @@ var Generate = (function($) {
         },
 
         bindEvents: function() {
-            this.form.bind('submit', this.formSubmit);
-            this.plus_button.on('click', this.addUse);
-            this.minus_button.on('click', this.subtractUse);
-            this.uses_input.on('blur', this.manualChange);
-            this.uses_input.on('keypress', this.inputKeyPress);
-        },
-
-        inputKeyPress: function(evt) {
-            evt = evt || window.event;
-            var charCode = evt.which || evt.keyCode;
-            var charStr = String.fromCharCode(charCode);
-            if (!/\d/.test(charStr)) {
-                return false;
-            }
-        },
-
-        manualChange: function(evt) {
-            if (Generate.uses_input.val() == "") {
-                Generate.uses_input.val(1);
-            }
-
-            var value = parseInt(Generate.uses_input.val());
-            if (!Generate.validateUses(value)) {
-                Generate.uses_input.val(1);
-            }
-        },
-
-        modifyUsage: function(change) {
-            var currentValue = parseInt(Generate.uses_input.val()),
-                newValue = currentValue + change;
-
-            if (Generate.validateUses(newValue)) {
-                Generate.uses_input.val(currentValue + change);
-            }
-        },
-
-        validateUses: function(value) {
-            if (value < 1) {
-                alert('Cannot have less than one usage.');
-                return false;
-            } else if (value > 50) {
-                alert("That's a lot of downloads. Why don't you just give out a public link?");
-                return false;
-            }
-            return true;
-        },
-
-        addUse: function() {
-            Generate.modifyUsage(1);
-        },
-
-        subtractUse: function() {
-            Generate.modifyUsage(-1);
-        },
-
-        formSubmit: function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            Generate.generateLink();
-        },
-
-        generateLink: function() {
-            var data = Generate.form.serialize();
-            $.ajax({
-                type: 'POST',
-                url: '/download',
-                data: data
-            }).done(function(data) {
-                Generate.displayLink(data.slug);
-            });
-        },
-
-        displayLink: function(link) {
-            Generate.url_output.val(window.location.origin + "/download/" + link);
-            Generate.url_output.focus();
-            Generate.url_output.select();
+            this.form.bind('submit', _formSubmit);
+            this.plus_button.on('click', _addUse);
+            this.minus_button.on('click', _subtractUse);
+            this.uses_input.on('blur', _manualChange);
+            this.uses_input.on('keypress', _inputKeyPress);
         }
     };
 
-    return Generate;
+    var _inputKeyPress = function(evt) {
+        evt = evt || window.event;
+        var charCode = evt.which || evt.keyCode;
+        var charStr = String.fromCharCode(charCode);
+        if (!/\d/.test(charStr)) {
+            return false;
+        }
+    };
+
+    var _manualChange = function(evt) {
+        if (generate.uses_input.val() == "") {
+            generate.uses_input.val(1);
+        }
+
+        var value = parseInt(generate.uses_input.val());
+        if (!generate.validateUses(value)) {
+            generate.uses_input.val(1);
+        }
+    };
+
+    var _modifyUsage = function(change) {
+        var currentValue = parseInt(generate.uses_input.val()),
+            newValue = currentValue + change;
+
+        if (generate.validateUses(newValue)) {
+            generate.uses_input.val(currentValue + change);
+        }
+    };
+
+    var _validateUses = function(value) {
+        if (value < 1) {
+            alert('Cannot have less than one usage.');
+            return false;
+        } else if (value > 50) {
+            alert("That's a lot of downloads. Why don't you just give out a public link?");
+            return false;
+        }
+        return true;
+    };
+
+    var _addUse = function() {
+        generate.modifyUsage(1);
+    };
+
+    var _subtractUse = function() {
+        generate.modifyUsage(-1);
+    };
+
+    var _formSubmit = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        generate.generateLink();
+    };
+
+    var _generateLink = function() {
+        var data = generate.form.serialize();
+        $.ajax({
+            type: 'POST',
+            url: '/download',
+            data: data
+        }).done(function(data) {
+            generate.displayLink(data.slug);
+        });
+    };
+
+    var _displayLink = function(link) {
+        generate.url_output.val(window.location.origin + "/download/" + link);
+        generate.url_output.focus();
+        generate.url_output.select();
+    };
+
+    var init = function() {
+        generate.init();
+    };
+
+    return {
+        init: init
+    };
 
 })(jQuery);
